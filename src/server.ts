adeb19ada4dba34b4443ce798c08f762d584c488
Gun Khemnak <gunnn_boy_hand@hotmail.com>
2026-01-29 12:05:52 +0700

    edit error uploda

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,6 @@
 import app from './app';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT ? parseInt(process.env.PORT, 10) : 3001;
 
 app.listen(PORT, '0.0.0.0', () => {
   console.log(`Server is running on http://0.0.0.0:${PORT}`);
